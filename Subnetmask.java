e2216a3497b2d5c42b11eceb2bc83c7b33d4e046
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2024-12-17 01:33:19 +0100

    Update Subnetmask.java

--- Subnetmask.java
+++ Subnetmask.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.subnet_rechner;
 
-/**
- *
- * @author Lorenz Schaller
- */
 public class Subnetmask {
 
     private int cidr;
